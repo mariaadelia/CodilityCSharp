451e3a1ce8960231e45976ff8d5b8b31743de00d
maria_adelia <maria.adelia.contato@gmail.com>
2024-01-05 09:45:49 -0300

    First codility challenge

---
+++ BinaryGap/BinaryGap/AnotherSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryGap
+{
+    /* Solution provided by VerxivrEltaire on
+     * https://github.com/VerxivrEltaire/BinaryGap/blob/master/BinaryGap/Program.cs 
+     */
+    public class AnotherSolution
+    {
+        public int Solution(int N)
+        {
+            int value = 0;
+            var results = new List<int>();
+            var binary = Convert.ToString(N, 2);
+
+            foreach (var c in binary)
+            {
+                if (c.Equals('0'))
+                {
+                    value += 1;
+                }
+                else
+                {
+                    results.Add(value);
+                    value = 0;
+                }
+            }
+
+            return results.Max();
+        }
+    }
+}
---
+++ BinaryGap/BinaryGap/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1610612737;
+            AnotherSolution sol = new AnotherSolution();
+            Console.WriteLine(sol.Solution(number));
+            MySolution sol2 = new MySolution();
+            Console.WriteLine(sol2.solution(number));
+
+        }
+    }
+
+    class MySolution
+    {
+
+        public int solution(int N)
+        {
+            // Implement your solution here
+            try
+            {
+                checkPositiveInteger(N);
+                var gapNumber = binaryGap(N);
+
+                return gapNumber;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+        }
+
+        public void checkPositiveInteger(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("The value cannot be negative");
+            }
+        }
+
+
+        public int binaryGap(int number)
+        {
+            string binaryNumber = Convert.ToString(number, 2);
+
+            var spNumber = new ArrayList();
+            var test = new List<int>();
+            string[] splitNumber = binaryNumber.Split('1');
+            int value = 0;
+
+            foreach (var num in splitNumber)
+            {
+                if (num != "")
+                    spNumber.Add(num);
+            }
+
+
+            //Checking if the length is bigger than 1 and if is not null
+            if (spNumber == null || spNumber.Count < 1)
+            {
+                return 0;
+            }
+            else
+            {
+                foreach (var num in spNumber)
+                {
+                    value = num.ToString().Length;
+                    test.Add(value);
+                }
+                return test.Max();
+            }
+
+
+        }
+    }
+}
